d4c2577c189db8bbd1d005bda34b2ec50a753585
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-05-02 16:22:54 -0700

    DRY up codebase
    Co-authored-by: William Rodriguez <william@rodriguez1.com>
    Co-authored-by: Nick Benzinger <Bennyz2030@users.noreply.github.com>

--- Riddles.cs
+++ Riddles.cs
@@ -6,15 +6,15 @@ public class Riddles
 {
   public static void Main()
   {
-    Console.WriteLine("Would you like to battle the Sphinx?");
+    Console.WriteLine("Would you like to battle the Sphinx? (y/n)");
     string answer = Console.ReadLine();
     if (answer == "Y" || answer == "y")
     {
       AskRiddles();
     }
-    else {
-      Console.WriteLine("You have no choice but to enter `y`.");
-      Main();
+    else
+    {
+      Console.WriteLine("Goodbye");
     }
   }
 
@@ -23,57 +23,36 @@ public class Riddles
     Random rand = new Random();
     Dictionary<string, string> riddleDictionary = new Dictionary<string, string> () { {"I run all around the pasture but never move. What am I?", "fence"}, {"What has roots as nobody sees, is taller than trees, up, up it goes, and yet never grows?", "mountain"}, {"Voiceless it cries, wingless flutters, toothless bites, mouthless mutters.", "wind"}, {"The more of this there is, the less you see.", "darkness"}, {"It stalks the countryside with ears that can't hear.", "corn"} };
 
-    KeyValuePair<string, string> riddleKey = riddleDictionary.ElementAt(rand.Next(riddleDictionary.Count));
-    string question = riddleKey.Key;
-    string answer = riddleKey.Value;
-
-    Console.WriteLine(question);
-    string riddleAnswer = Console.ReadLine().ToLower();
-    if (riddleAnswer == answer)
+    bool alive = true;
+    while (riddleDictionary.Count > 0 && alive)
     {
-      Console.WriteLine("Correct. You shall pass.");
-      riddleDictionary.Remove(question);
-      KeyValuePair<string, string> riddleKey2 = riddleDictionary.ElementAt(rand.Next(riddleDictionary.Count));
-      string question2 = riddleKey2.Key;
-      string answer2 = riddleKey2.Value;
+      KeyValuePair<string, string> riddleKey = riddleDictionary.ElementAt(rand.Next(riddleDictionary.Count));
+      string question = riddleKey.Key;
+      string answer = riddleKey.Value;
 
-      Console.WriteLine(question2);
-      string riddleAnswer2 = Console.ReadLine().ToLower();
-      if (riddleAnswer2 == answer2)
+      if (AskQuestion(question, answer))
       {
         Console.WriteLine("Correct. You shall pass.");
-        riddleDictionary.Remove(question2);
-        KeyValuePair<string, string> riddleKey3 = riddleDictionary.ElementAt(rand.Next(riddleDictionary.Count));
-        string question3 = riddleKey3.Key;
-        string answer3 = riddleKey3.Value;
-        
-        Console.WriteLine(question3);
-        string riddleAnswer3 = Console.ReadLine().ToLower();
-        if (riddleAnswer3 == answer3)
-        {
-          Console.WriteLine("Correct. You have defeated the Sphinx!");
-        } 
-        else 
-        {
-          WrongAnswer();
-        }
-      } 
-      else 
-      {
-        WrongAnswer();
+        riddleDictionary.Remove(question);
       }
-    } 
-    else 
-    {
-      WrongAnswer();
+      else
+      {
+        Console.WriteLine("Incorrect.");
+        Console.WriteLine("*User gets eaten.*");
+        alive = false;
+      }
     }
-    
+
+    if (alive)
+    {
+      Console.WriteLine("You defeated the Sphinx!");
+    }
   }
-  
-  static void WrongAnswer()
+
+  private static bool AskQuestion(string question, string answer)
   {
-    Console.WriteLine("Incorrect.");
-    Console.WriteLine("*User gets eaten.*");
-    Main();
+    Console.WriteLine(question);
+    string riddleAnswer = Console.ReadLine().ToLower();
+    return riddleAnswer == answer;
   }
 }
