773d6f178f3ee628c518af3bc1649ecd0131c373
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-05-02 14:28:57 -0700

    added more riddles
    Co-authored-by: William Rodriguez <william@rodriguez1.com>
    Co-authored-by: Marcus Lorenzo <marcus.kaitara.lorenzo@gmail.com>

--- Riddles.cs
+++ Riddles.cs
@@ -21,7 +21,7 @@ public class Riddles
   static void AskRiddles()
   {
     Random rand = new Random();
-    Dictionary<string, string> riddleDictionary = new Dictionary<string, string> () { {"I run all around the pasture but never move. What am I?", "fence"}, {"What has roots as nobody sees, is taller than trees, up, up it goes, and yet never grows?", "mountain"}, {"Voiceless it cries, wingless flutters, toothless bites, mouthless mutters.", "wind"} };
+    Dictionary<string, string> riddleDictionary = new Dictionary<string, string> () { {"I run all around the pasture but never move. What am I?", "fence"}, {"What has roots as nobody sees, is taller than trees, up, up it goes, and yet never grows?", "mountain"}, {"Voiceless it cries, wingless flutters, toothless bites, mouthless mutters.", "wind"}, {"The more of this there is, the less you see.", "darkness"}, {"It stalks the countryside with ears that can't hear.", "corn"} };
 
     KeyValuePair<string, string> riddleKey = riddleDictionary.ElementAt(rand.Next(riddleDictionary.Count));
     string question = riddleKey.Key;
